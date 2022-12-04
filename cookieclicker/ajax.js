1624779bfa7fc5553e35afa8d0f9b8331814caf8
gxpfhog <testdummysayshello@gmail.com>
2022-12-03 19:11:57 -0500

    tried fixing something

--- cookieclicker/ajax.js
+++ cookieclicker/ajax.js
@@ -3,4 +3,4 @@ function ajax(url,callback){
 	try{ajaxRequest = new XMLHttpRequest();} catch (e){try{ajaxRequest=new ActiveXObject('Msxml2.XMLHTTP');} catch (e) {try{ajaxRequest=new ActiveXObject('Microsoft.XMLHTTP');} catch (e){alert("Something broke!");return false;}}}
 	if (callback){ajaxRequest.onreadystatechange=function(){if(ajaxRequest.readyState==4){callback(ajaxRequest.responseText);}}}
 	ajaxRequest.open('GET',url+'&nocache='+(new Date().getTime()),true);ajaxRequest.send(null);
-}
+}3
